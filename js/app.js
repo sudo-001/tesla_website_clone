336eebd04c5376e216348ff3f12b1c1bf921e481
sudo-001 <carick.atezong@facsciences-uy1.cm>
2022-10-08 17:47:04 +0100

    ajout du style flex à la navbar

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let navigation = document.querySelector('#navigation')
+let menuButton = document.querySelector('#menuButton')
+
+menuButton.addEventListener('click', () => {
+    if(!navigation.classList.contains('block')) {
+        
+    } else {
+        console.log('no')
+    }
+})
